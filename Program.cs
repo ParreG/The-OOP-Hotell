6f91df0472f2c4cc0d6de7cb31b43de49fd2e7a6
Parman Gitijah <parman.gitijah@chasacademy.se>
2024-09-24 14:06:23 +0200

    med pris, rabatt, och ny klass för person

--- Program.cs
+++ Program.cs
@@ -3,37 +3,78 @@ using System.Reflection;
 
 namespace The_OOP_Hotell
 {
+
+
+    class Person
+    {
+        public string[] GuesstName { get; set; }
+        public string Email { get; set; }
+        public string Tel { get; set; }
+
+        public Person(string[] guesstName, string email, string tel)
+        {
+            GuesstName = guesstName;
+            Email = email;
+            Tel = tel;
+        }
+
+        
+    }
     class HotelBooking
     {
-        public string GuesstName { get; set; }
+        public Person Person { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
 
         public int lengthOfStayInNights;
 
-        public int price;
+        public double price;
 
-        public HotelBooking(string guesstName, DateTime startDate, int lengthOfStayInDays)
+        public HotelBooking (Person person, DateTime startDate, int lengthOfStayInDays)
         {
-            GuesstName = guesstName;
+            Person = person;
             StartDate = startDate.Add(new TimeSpan(15, 0, 0));
             EndDate = startDate.AddDays(lengthOfStayInDays).Date.Add(new TimeSpan(12, 0, 0));
             lengthOfStayInNights = lengthOfStayInDays;
 
         }
 
-        public int Price(int lengthOfStayInDays)
+        public double Price(int lengthOfStayInDays)
         {
             price = 1999;
+            if (lengthOfStayInDays < 8 && lengthOfStayInDays > 5)
+            {
+                price = price * 0.9;
+            }
+            else if (lengthOfStayInDays < 10)
+            {
+                price = price * 0.8;
+            }
+            else if (lengthOfStayInDays < 18)
+            {
+                price = price * 0.6;
+            }
+            else
+            {
+                price = price * 0.5;
+            }
+
             return lengthOfStayInDays * price;
         }
 
         public void information()
         {
-            Console.WriteLine("Hej och välkommen mr/ms " + GuesstName);
-            Console.WriteLine("Du har bokat ditt hotell rum från " + StartDate);
+            Console.WriteLine("Hej och välkommen mr/ms " + Person.GuesstName[0] + ", " + Person.Tel + ", " + Person.Email);
+            Console.WriteLine("Du har bokat ett rum för följande namn: ");
+            foreach ( var name in Person.GuesstName)
+            {
+                Console.WriteLine(name + ".");
+            }
+            
+            Console.WriteLine("Ditt rum är bokat från " + StartDate);
             Console.WriteLine("Till och med: " + EndDate);
-            Console.WriteLine("Ditt totala pris per natt är: " + Price(lengthOfStayInNights) + "kr");
+            Console.WriteLine("Ditt totala pris är " + Price(lengthOfStayInNights) + "kr");
+            Console.WriteLine();
         }
 
         
@@ -46,8 +87,26 @@ namespace The_OOP_Hotell
         static void Main(string[] args)
         {
             Console.WriteLine("Hej och välkommen till OOP Hotellet.");
-            Console.WriteLine("Vad heter du? För/Efternamn");
-            string name = Console.ReadLine();
+            Console.WriteLine("Hur många gäster vill du boka rum för? ");
+            int numberOfGuests = Convert.ToInt32(Console.ReadLine());
+
+            string[] names = new string[numberOfGuests];
+
+            Console.WriteLine("Nu ska du få skria namn får alla gäster. Första namnet du skriver in ska vara namnet för bokningen!");
+            for (int i = 0; i < numberOfGuests; i++)
+            {
+                Console.WriteLine($"Skriv namn på gäst nummer {i+1}");
+                names[i] = Console.ReadLine();
+            }
+ 
+            Console.WriteLine("Skriv in ett mejladress för bookningen: ");
+            String mejl = Console.ReadLine();
+
+            Console.WriteLine("skriv in ditt telefonnummer: ");
+            string tel = Console.ReadLine();
+
+            Person guest = new Person(names, mejl, tel);
+
 
             Console.WriteLine("När vill du boka rum hos oss? Start Datum: ÅÅÅÅ-MM-DD ");
             DateTime startDate;
@@ -60,7 +119,7 @@ namespace The_OOP_Hotell
             Console.WriteLine("Hur många dagar vill du stanna hos oss? Skriv antal dagar med nummer ex 12: ");
             int lengthOfDays = Convert.ToInt32(Console.ReadLine());
 
-            HotelBooking booking = new HotelBooking(name, startDate, lengthOfDays);
+            HotelBooking booking = new HotelBooking(guest, startDate, lengthOfDays);
             booking.information();
 
             Console.WriteLine("Vill du ändra din bokning? ");
@@ -70,7 +129,7 @@ namespace The_OOP_Hotell
             {
                 Console.WriteLine("Hur många dagar vill du ändra det till? ");
                 lengthOfDays = Convert.ToInt32(Console.ReadLine());
-                HotelBooking hotelBooking = new HotelBooking(name, startDate, lengthOfDays);
+                HotelBooking hotelBooking = new HotelBooking(guest, startDate, lengthOfDays);
                 Console.WriteLine("Nu har vi uppdaterat din bookning. Nedan följer information för din bookning");
                 hotelBooking.information();
             }
